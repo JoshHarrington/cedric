df789b877e6d75c96de62681d5cf96ea12293c4d
Josh Harrington <josh@joshharrington.co.uk>
2018-10-03 17:20:15 +0100

    updates to icons

--- background.js
+++ background.js
@@ -20,8 +20,6 @@ function turnEditingOn() {
 	var downloadButtonText = document.createTextNode('Download this page');
 	downloadButton.appendChild(downloadButtonText);
 
-  // var downloadButton = document.createElement('<a id="download_btn" href="#" download="page.html" style="position:fixed; bottom: 10px; right: 10px; background-color: #222; border: none; padding: 10px 12px; font-size: 14px; color: white; font-family: sans-serif; text-decoration: none; z-index: 9999;">Download this page</a>');
-
   downloadButton.onclick = function(e){
     turnEditingOff(d);
     var pageHtml = 'data:text/html;charset=UTF-8,' + encodeURIComponent(document.documentElement.outerHTML);
@@ -32,21 +30,9 @@ function turnEditingOn() {
 };
 
 
-// document.addEventListener('DOMContentLoaded', function() {
-// 	var editBtn = document.getElementById('start_edit')
-//   editBtn.addEventListener('click', function() {
-// 		chrome.activeTab(null, function(tab) {
-// 			d = tab.document;
-// 			turnEditingOn(d);
-// 		});
-// 	})
-// });
-
-chrome.browserAction.onClickedocument.addListener(function(tab) {
-  // No tabs or host permissions needed!
-	console.log('Turning ' + tab.url + ' editable!');
+chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.executeScript({
-    code: 'turnEditingOn()'
+    code: '(' + turnEditingOn.toString() + ')();'
   });
 });
 
